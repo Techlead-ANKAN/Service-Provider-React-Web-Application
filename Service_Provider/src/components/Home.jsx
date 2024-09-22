4c1cd425cab69664da9fc208ad75ea966893a8f9
ANKAN MAITY <mr.ankanmaity@gmail.com>
2024-09-22 12:49:54 +0530

    Done with the Add service functionality. Delete Service Functionality, partially done with the update service functionality

--- Service_Provider/src/components/Home.jsx
+++ Service_Provider/src/components/Home.jsx
@@ -6,6 +6,10 @@ function Home() {
 
     const [addipvisible, setipvisibility] = useState(false);
 
+    const [del_statement, setdelstatement] = useState(false);
+
+    const [update_visible, setupdatevisible] = useState(false);
+
     const [newService, setNewService] = useState({ name: "", desc: "", price: "" })
 
     function Add_btn() {
@@ -29,11 +33,25 @@ function Home() {
         let new_service = document.createElement("div");
         new_service.setAttribute("class", "service");
 
-        // new_service.innerHTML = `
-        // <p>Name: ${name.value}</p>
-        // <p>Desc: ${desc.value}</p>
-        // <p>Price: ${price.value}/-</p>
-        // `
+        let name_price = document.createElement("div");
+        name_price.setAttribute("class", "name_price");
+        let p1 = document.createElement("p");
+        p1.textContent = name.value;
+        name_price.appendChild(p1);
+        let p2 = document.createElement("p");
+        p2.textContent = price.value + "/-";
+        name_price.appendChild(p2);
+
+
+        let description = document.createElement("div");
+        description.setAttribute("class", "description");
+        let text_ele = document.createElement("textarea");
+        text_ele.value = desc.value;
+        description.appendChild(text_ele);
+
+
+        new_service.appendChild(name_price);
+        new_service.appendChild(description);
 
         row_ele.appendChild(new_service);
 
@@ -43,13 +61,30 @@ function Home() {
 
     }
 
+    function Del_btn(){
+        setdelstatement(true);
+      
+        const rowContainer = document.querySelector(".row"); // Select the parent container
+      
+        rowContainer.addEventListener('click', event => {
+          if (event.target.classList.contains("service")) {
+            event.target.remove(); // Remove the .service container
+          }
+        });
+    }
+
+    function Update_btn() {
+        setupdatevisible(true);
+    }
+
+
     return (
         <>
 
             <div class="home">
                 <button class="add btn" onClick={Add_btn}>Add Services</button>
-                <button class="delete btn">Delete Services</button>
-                <button class="update btn">Update Services</button>
+                <button class="delete btn" onClick={Del_btn}>Delete Services</button>
+                <button class="update btn" onClick={Update_btn}>Update Services</button>
             </div>
 
 
@@ -67,8 +102,38 @@ function Home() {
                     </div>
                 ) : null}
 
+                {del_statement ? (
+                    <div class="del_line" >
+                        Select Services to delete
+                    </div>
+                ) : null}
+
+
+                {update_visible ? (
+                    <div class="update_ip" >
+                        <label>New Name: </label>
+                        <input class="name" type="text" placeholder="Enter Service name here" />
+                        <label>New Desc: </label>
+                        <input class="desc" type="text" placeholder="Enter Service description here" />
+                        <label>New Price: </label>
+                        <input class="price" type="text" placeholder="Enter Service price here" />
+                        <button class="update_btn" onClick={Add_Service}>Update</button>
+                    </div>
+                ) : null}   
+
                 <div className="row">
+                    {/* <div className="service">
+                        <div className="name_price">
+                            <p>MRI SCAN</p>
+                            <p>10000/-</p>
+                        </div>
+                        <div className="description">
+                            <textarea name="" id="" readOnly></textarea>
+                        </div>
+                    </div> */}
                 </div>
+
+
             </div>
 
         </>
@@ -76,3 +141,4 @@ function Home() {
 }
 
 export default Home;
+
